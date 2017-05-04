78df6b05047e433bdf59e064579fc64042f6da64
Mariam Ben-Neticha <Mariam@Mariams-MacBook-Pro.local>
2017-05-04 12:01:42 -0400

    initial setup

---
+++ oauthDemo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from google.appengine.ext import ndb
+import logging
+import webapp2
+import json
+
+class OauthHandler(webapp2.RequestHandler):
+	def get(self):
+		logging.debug('The contents of the GET request are:' + repr(self.request.GET))
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		self.response.write("hello")
+
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+	('/oauth', OauthHandler)
+], debug=True)
