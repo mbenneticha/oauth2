98e3bd0a90935f669f51cac3f0afc3ae043a05f3
Mariam Ben-Neticha <Mariam@Mariams-MacBook-Pro.local>
2017-05-07 18:03:51 -0400

    fixes errors

--- oauthDemo.py
+++ oauthDemo.py
@@ -24,30 +24,28 @@ class OauthHandler(webapp2.RequestHandler):
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		#self.response.write("hello, good-bye, adele.")
-		guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
+		guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
+		greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+		greetings = greetings_query.fetch(10)
 
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+		user = users.get_current_user()
+		if user:
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
 
-        template_values = {
-            'user': user,
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-        }
+		template_values = {
+			'user': user,
+			'greetings': greetings,
+			'guestbook_name': urllib.quote_plus(guestbook_name),
+			'url': url,
+			'url_linktext': url_linktext,
+		}
 
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
 
 
 
