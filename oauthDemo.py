3331212264406aaa5c62ce3408c87414d10f79ab
Mariam Ben-Neticha <Mariam@Mariams-MacBook-Pro.local>
2017-05-07 20:14:36 -0400

    moving forward

--- oauthDemo.py
+++ oauthDemo.py
@@ -18,17 +18,18 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class OauthHandler(webapp2.RequestHandler):
 	def get(self):
-		user = users.get_current_user()
-		if user:
-			nickname = user.nickname()
-			logout_url = users.create_logout_url('/')
-			greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format( nickname, logout_url)
-		else:
-			login_url = users.create_login_url('/')
-			greeting = '<a href="{}">Sign in</a>'.format(login_url)
+		code = self.request.get('code')
+		state = self.request.get('state') 
+		cid = '539287248398-hsq5ikfp43tem0edsfr2hl02otp01d6t.apps.googleusercontent.com'
+		sec = 'l_5sBiVk8S4MRdxj7CSOjN5L'
+		redirect = 'https://python-gae-quickstart-164103.appspot.com/oauth'
+		gt = 'authorization_code'
+		head = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-		self.response.write('<html><body>{}</body></html>'.format(greeting))
-		logging.debug('The contents of the GET request are:' + repr(self.request.GET))
+		data={'code':code,'client_id':cid,'client_secret':sec,'redirect_uri':redirect,'grant_type':gt}
+		result = urlfetch.fetch(url='https://www.googleapis.com/oauth2/v4/token',payload=urllib.urlencode(data),method=urlfetch.POST,headers=head)
+
+
 
 
 
@@ -37,12 +38,31 @@ class MainPage(webapp2.RequestHandler):
 		title = 'OAuth 2.0 Implementation'
 		course = 'CS 496'
 		description = 'By clicking on the following link, you are authorizing access to your name and email from Google. Click on the link below to authorize Google to give me that information. After clicking the link, we will display your full name, the URL we used to access your Google+ Account and the randomly generated token used to authorize access.'
-		redirect_url = 'https://python-gae-quickstart-164103.appspot.com/oauth'
+		redirect_url = 'https://accounts.google.com/o/oauth2/v2/auth'
 		link = 'Go To (<a href="{}">Google</a>)'.format(redirect_url)
 		greeting = '{} \n {} \n {} \n {}'.format(title, course, description, link)
 		self.response.write('<html><body>{}</body></html>'.format(greeting))
 		#path = os.path.join(os.path.dirname(__file__), 'index.html')
 		#self.response.out.write(path)
+
+	def post(self):
+		headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+		PARAMS = {
+                'code': code,
+                'client_id': client_id,
+                'client_secret': client_secret,
+          }
+          try:
+          	result = urlfetch.fetch(
+          		url=HTTP_REQUEST_URL,
+                payload=urllib.urlencode(PARAMS),
+                method=urlfetch.POST/GET/DELETE/...,
+                headers=headers
+                )
+          except urlfetch.Error as e:
+          	logging.debug(e)
+
+          self.response.write(json.loads(result.content))
 
 
 
